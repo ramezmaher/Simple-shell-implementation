b741a5af7a360064bb84d6e408c5d39585ca0f6a
Ramez Maher <ramezmaher@hotmail.com>
2020-11-05 01:07:04 +0200

    Input ready for operations

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 //Used for debugging
 void print_args(char* str[]){
     int i=0;
-    while(str[i] != "\0"){
+    while(str[i] != NULL){
         printf("%s\n",str[i++]);
     }
 
@@ -17,21 +17,21 @@ void print_args(char* str[]){
 int divide_string(char input[],char* arr[]){
     int i=0;
     arr[i] = strtok(input," ");
-    while(arr[i] != "\0"){
+    while(arr[i] != NULL){
         i++;
         arr[i] = strtok(NULL," ");
-        printf("%s",arr[i]);
     }
-    return 1;
+    return i;
 }
 
 //This method loops over a string until it finds the newLine character and then replaces it with a null character
-void delete_newLine(char str[]){
+int delete_newLine(char str[]){
     int i=0;
     while(str[i] != '\n'){
         i++;
     }
     str[i] = '\0';
+    return i;
 }
 
 int main(){
@@ -39,12 +39,17 @@ int main(){
     //variables definitions 
     char inputStr[inLen]; //stores input command given by the user 
     char* args[argLen]; //stores all the arguments of the input command eg. args[0]="ls",args[1]=[-a]..
+    int VerLen,VerArg; //Those values are meant to test whether the input is out of the variables bounds
 
     //Taking the input and preparing it for the operations
     fgets(inputStr,inLen,stdin); //Takes input 
-    delete_newLine(inputStr); //Removes the newLine character from the end of the input string 
-    divide_string(inputStr,args); //Divide the string into smaller substrings & forming the arguments array of strings
-    print_args(args);
+    VerLen = delete_newLine(inputStr); //Removes the newLine character from the end of the input string 
+    VerArg = divide_string(inputStr,args); //Divide the string into smaller substrings & forming the arguments array of strings
+    if(VerLen >= inLen || VerArg >= argLen){
+        printf("Invalid command\n");
+        return 0;
+    } // checks if the input is out of the size bound
+    
 
     return 0;
 }
